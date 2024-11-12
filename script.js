70d12c563b7c2cd39b8f215be79510d837f1dd02
Aiu Jymph Yap <186591613+aiuyap@users.noreply.github.com>
2024-11-13 00:22:25 +0800

    Project completed!
    - changed font

--- script.js
+++ script.js
@@ -24,7 +24,7 @@ function modolusFunction() {
 
 }
 function getDisplayNumber () {
-    return parseInt(displayArea.textContent);
+    return +displayArea.textContent;
 }
 function processAfterPressingOperator () {
     firstNum = getDisplayNumber();
